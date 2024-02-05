9abaed92b286e3aa092bf89b05c6605fd51127bf
Naftali Ferreira <108247621+naftaliferreira@users.noreply.github.com>
2024-02-04 23:15:50 -0300

    Conclusão do projeto1
    Foi adicionado tambem um documento PDF com a parte teórica da aula.

--- Projeto1_appAudioBook/script.js
+++ Projeto1_appAudioBook/script.js
@@ -9,49 +9,49 @@ let taTocando = false;
 let capitulo = 1;
 
 function tocarFaixa() {
-    botaoPlayPause.classList.remove("bi-play-fill");
-    botaoPlayPause.classList.add("bi-pause-fill");
-    audio.play();
-    taTocando = true;
-  }
+  botaoPlayPause.classList.remove("bi-play-circle-fill");
+  botaoPlayPause.classList.add("bi-pause-circle-fill");
+  audio.play();
+  taTocando = true;
+}
 
 function pausarFaixa() {
-    botaoPlayPause.classList.add("bi-play-fill");
-    botaoPlayPause.classList.remove("bi-pause-fill");
-    audio.pause();
-    taTocando = false;
-  }
+  botaoPlayPause.classList.add("bi-play-circle-fill");
+  botaoPlayPause.classList.remove("bi-pause-circle-fill");
+  audio.pause();
+  taTocando = false;
+}
 
 function tocarOuPausarFaixa() {
-    if (taTocando === true) {
-      pausarFaixa();
-    } else {
-      tocarFaixa();
-    }
+  if (taTocando === true) {
+    pausarFaixa();
+  } else {
+    tocarFaixa();
   }
+}
 
 function capituloAnterior() {
-    if (capitulo === 1) {
-      capitulo = quantidadeCapitulos;
-    } else {
-      capitulo -= 1;
-    }
-    audio.src = "books/dom-casmurro/" + capitulo + ".mp3";
-    nomeCapitulo.innerText = "Capítulo " + capitulo;
-    tocarFaixa();
+  if (capitulo === 1) {
+    capitulo = quantidadeCapitulos;
+  } else {
+    capitulo -= 1;
   }
-  
+  audio.src = "books/dom-casmurro/" + capitulo + ".mp3";
+  nomeCapitulo.innerText = "Capítulo " + capitulo;
+  tocarFaixa();
+}
+
 function proximoCapitulo() {
-    if (capitulo < quantidadeCapitulos) {
-      capitulo += 1;
-    } else {
-      capitulo = 1;
-    }
-    audio.src = "books/dom-casmurro/" + capitulo + ".mp3";
-    nomeCapitulo.innerText = "Capítulo " + capitulo;
-    tocarFaixa();
+  if (capitulo < quantidadeCapitulos) {
+    capitulo += 1;
+  } else {
+    capitulo = 1;
   }
-  
+  audio.src = "books/dom-casmurro/" + capitulo + ".mp3";
+  nomeCapitulo.innerText = "Capítulo " + capitulo;
+  tocarFaixa();
+}
+
 botaoPlayPause.addEventListener("click", tocarOuPausarFaixa);
 botaoCapituloAnterior.addEventListener("click", capituloAnterior);
 botaoProximoCapitulo.addEventListener("click", proximoCapitulo);
